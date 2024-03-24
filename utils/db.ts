a573d6afab191be6dd039d76824d5088a0680948
Paweł Benkowski <pawlo97.pb@gmail.com>
2024-03-24 11:54:38 +0100

    Setup db

---
+++ utils/db.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client'
+
+// There is this globalThis thing in node
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined
+  }
+  
+// If it's not already attach, attach it
+export const prisma =
+    globalForPrisma.prisma ??
+        new PrismaClient({
+            log: ['query'],
+        })
+
+// Prevent Next.js from dying after 10 hot reloads
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
